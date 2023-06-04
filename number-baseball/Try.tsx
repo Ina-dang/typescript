96212de8dd3666a827adf70022f6c365354baa15
Ina-dang <ina9377@gmail.com>
2023-06-05 06:46:19 +0900

    20230605 글자야구 만들기 (진행중)

---
+++ number-baseball/Try.tsx
@@ -0,0 +1,14 @@
+import * as React from "react"
+import { FunctionComponent } from "react"
+// import {TryInfo} from './NumberBaseball'
+
+const Try: FunctionComponent = ({}) => {
+  return (
+    <li>
+      {/* <div>{tryInfo.try}</div>
+       <div>{tryInfo.result}</div> */}
+    </li>
+  )
+}
+
+export default Try
