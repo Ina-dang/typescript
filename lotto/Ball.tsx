8847c7b071ebccfdba0a25ab8615fdf1066abd59
Ina-dang <ina9377@gmail.com>
2023-06-13 07:15:53 +0900

    useEffect추가, 1초에 1개씩 출력

--- lotto/Ball.tsx
+++ lotto/Ball.tsx
@@ -1,7 +1,24 @@
-import React, { memo } from "react"
+import * as React from "react"
+import { FunctionComponent, memo } from "react"
 
-const Ball = () => {
-  return <></>
+const Ball: FunctionComponent<{ number: number }> = ({ number }) => {
+  let background
+  if (number <= 10) {
+    background = "red"
+  } else if (number <= 20) {
+    background = "orange"
+  } else if (number <= 30) {
+    background = "yellow"
+  } else if (number <= 40) {
+    background = "blue"
+  } else {
+    background = "green"
+  }
+  return (
+    <div className="ball" style={{ background }}>
+      {number}
+    </div>
+  )
 }
 
 export default Ball
--- lotto/Lotto.tsx
+++ lotto/Lotto.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
-import { useState, useRef, useMemo, useCallback } from "react"
+import { useState, useRef, useMemo, useCallback, useEffect } from "react"
+import Ball from "./Ball"
 
 function getWinNumbers() {
   console.log("getWinNumbers")
@@ -13,21 +14,44 @@ function getWinNumbers() {
     )
   }
   const bonusNumber = shuffle[shuffle.length - 1]
-  const winNumbers = shuffle.slice(0.6).sort((p, c) => p - c)
+  const winNumbers = shuffle.slice(0, 6).sort((p, c) => p - c)
   return [...winNumbers, bonusNumber]
 }
-
 const Lotto = () => {
   const lottoNumbers = useMemo(() => getWinNumbers(), [])
   const [winNumbers, setWinNumbers] = useState(lottoNumbers)
-  const [winBalls, setWinballs] = useState<number[]>([])
+  const [winBalls, setWinBalls] = useState<number[]>([])
   const [bonus, setBonus] = useState<number | null>(null)
   const [redo, setRedo] = useState(false)
   const timeouts = useRef<number[]>([])
 
+  useEffect(() => {
+    for (let i = 0; i < winNumbers.length; i++) {
+      timeouts.current[i] = window.setTimeout(() => {
+        setWinBalls((preBalls) => [...preBalls, winNumbers[i]])
+      }, (i + 1) * 1000)
+    }
+
+    timeouts.current[6] = window.setTimeout(() => {
+      setBonus(winNumbers[6])
+      setRedo(true)
+    }, 7000)
+
+    return () => {
+      timeouts.current.forEach((v) => {
+        clearTimeout(v)
+      })
+    }
+  }, [timeouts.current]) // 빈 배열이면 componentDidMount와 동일
+  // 배열에 요소가 있으면 componentDidMount랑 componentDidUpdate 둘 다 수행
+
+  useEffect(() => {
+    console.log("로또 숫자를 생성합니다.")
+  }, [winNumbers])
+
   const onClickRedo = useCallback(() => {
     setWinNumbers(getWinNumbers())
-    setWinballs([])
+    setWinBalls([])
     setBonus(null)
     setRedo(false)
     timeouts.current = []
@@ -41,7 +65,7 @@ const Lotto = () => {
           <Ball key={v} number={v} />
         ))}
       </div>
-      <div>보너스</div>
+      <div>보너스!</div>
       {bonus && <Ball number={bonus} />}
       {redo && <button onClick={onClickRedo}>한 번 더!</button>}
     </>
