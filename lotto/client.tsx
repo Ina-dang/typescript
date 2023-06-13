d00780e84f724e518ac9a21411b89a7c5c01d0b1
Ina-dang <ina9377@gmail.com>
2023-06-14 07:10:10 +0900

    로또 함수 컴포넌트를 클래스 컴포넌트로도 만들어보기

--- lotto/client.tsx
+++ lotto/client.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
 
-import Lotto from "./Lotto"
+import LottoClass from "./LottoClass"
 
-ReactDOM.render(<Lotto />, document.querySelector("#root"))
+ReactDOM.render(<LottoClass />, document.querySelector("#root"))
