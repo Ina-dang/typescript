e4f64f5cb4fa6bb78b63e98460f244d13e512e72
Ina-dang <ina9377@gmail.com>
2023-06-15 06:55:10 +0900

    리듀서, 액션 생성

--- tictacto/TicTacToe.tsx
+++ tictacto/TicTacToe.tsx
@@ -1,7 +1,111 @@
 import * as React from "react"
+import { useEffect, useCallback, useReducer, Reducer } from "react"
+// import Table from "./Table"
+
+interface ReducerState {
+  winner: "O" | "X" | ""
+  turn: "O" | "X"
+  tableData: String[][]
+  recentCell: [number, number]
+}
+
+const initialState: ReducerState = {
+  winner: "",
+  turn: "O",
+  tableData: [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ],
+  recentCell: [-1, -1],
+}
+
+export const SET_WINNER = "SET_WINNER"
+export const CLICK_CELL = "CLICK_CELL" as const
+export const CHANGE_TURN = "CHANGE_TURN" as const
+export const RESET_GAME = "RESET_GAME"
+
+interface SetWinnerAction {
+  type: typeof SET_WINNER
+  winner: "O" | "X"
+}
+
+const setWinner = (winner: "O" | "X"): SetWinnerAction => {
+  return { type: SET_WINNER, winner }
+}
+
+interface ClickCellAction {
+  type: typeof CLICK_CELL
+  row: number
+  cell: number
+}
+
+const clickCell = (row: number, cell: number): ClickCellAction => {
+  return { type: CLICK_CELL, row, cell }
+}
+
+interface ChangeTurnAction {
+  type: typeof CHANGE_TURN
+}
+
+interface ResetGameAction {
+  type: typeof RESET_GAME
+}
+
+type ReducerActions =
+  | SetWinnerAction
+  | ClickCellAction
+  | ChangeTurnAction
+  | ResetGameAction
+const reducer = (state: ReducerState, action: ReducerActions): ReducerState => {
+  switch (action.type) {
+    case SET_WINNER:
+      //state.winner = action.winner =>이렇게 사용하면 안됨
+      return {
+        ...state,
+        winner: action.winner,
+      }
+      break
+    case CLICK_CELL:
+      const tableData = [...state.tableData]
+      tableData[action.row] = [...tableData[action.row]] //나중에 immer사용해서 줄일예정
+      tableData[action.row][action.cell] = state.turn
+      return {
+        ...state,
+        tableData,
+        recentCell: [action.row, action.cell],
+      }
+    case CHANGE_TURN: {
+      return {
+        ...state,
+        turn: state.turn === "O" ? "X" : "O",
+      }
+    }
+    case RESET_GAME: {
+      return {
+        ...state,
+        turn: "O",
+        tableData: [
+          ["", "", ""],
+          ["", "", ""],
+          ["", "", ""],
+        ],
+        recentCell: [-1, -1],
+      }
+    }
+    default:
+      return state
+      break
+  }
+}
 
 const TicTacToe = () => {
-  return <></>
+  return (
+    <>
+      {/* <Table onClick={onClickTable} tableData={tableData} dispatch={dispatch} />
+      {winner && <div>{winner}님의 승리</div>} */}
+    </>
+  )
 }
 
 export default TicTacToe
