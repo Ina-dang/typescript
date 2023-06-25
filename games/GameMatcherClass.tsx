f3159790b1da885fa389d875d9f5650ccffe5879
Ina-dang <ina9377@gmail.com>
2023-06-26 07:04:03 +0900

    게임들을 한 페이지에서 이동할 수 있게 games 폴더 생성 후 라우터 설치

---
+++ games/GameMatcherClass.tsx
@@ -0,0 +1,10 @@
+import * as React from "react"
+import { Component } from "react"
+import NumberBaseBall from "../number-baseball/NumberBaseballClass"
+import RSP from "../rsp/RSPClass"
+import Lotto from "../lotto/LottoClass"
+
+class GameMatcherClass extends Component {
+  render() {}
+}
+export default GameMatcherClass
