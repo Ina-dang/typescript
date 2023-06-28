3fba8da1f61558b447bf0b465b463c4eb19ea507
Ina-dang <ina9377@gmail.com>
2023-06-29 06:50:00 +0900

    자바스크립트로 리덕스 기본 틀 잡아두기 (스토어,리듀서,액션)

--- games/client.tsx
+++ games/client.tsx
@@ -1,11 +1,12 @@
 import * as React from "react"
-import ReactDOM from "react-dom" // react v17 버전용
-
-import Games from "./Games"
+import ReactDOM from "react-dom"
+import { Provider } from "react-redux"
+import store from "./store"
+import App from "./App"
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Games />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById("root")
 )
