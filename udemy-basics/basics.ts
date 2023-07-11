bbd93082325c4a46811ea07bbf4cd0435be6e296
Ina-dang <ina9377@gmail.com>
2023-07-12 06:25:52 +0900

    타입스크립트 타입추론, 유니온타입

--- udemy-basics/basics.ts
+++ udemy-basics/basics.ts
@@ -10,4 +10,40 @@ let isInstructor: boolean
 
 isInstructor = true
 
-let hobbies: null
+let hobbies: string[]
+
+hobbies = ["Sports", "Cooking"]
+
+let person: {
+  name: string
+  age: number
+}
+person = {
+  name: "Max",
+  age: 32,
+}
+
+let people: (typeof person)[]
+
+people = [
+  {
+    name: "Max",
+    age: 32,
+  },
+  {
+    name: "Laura",
+    age: 22,
+  },
+]
+
+//Type infrrance (타입 추론)
+
+// let course: string = 'React 0 The Complete Guide'
+let course: string | number = "React 0 The Complete Guide"
+course = 1234
+/**
+ * Type 'number' is not assignable to type 'string' 오류가 나는 이유
+ * 타입스크립트는 명시적인 타입표기가 없어도 가능한 많은 타입을 추론하려한다.
+ * 따라서 불필요하게 타입을 지정하지않고 타입 추론을 최대한 많이 이용하는게 좋다.
+ * 만약 강좌명처럼 강좌이름 또는 강좌 번호를 써야하는경우는 위처럼 유니온타입 | 을 이용해서 타입을 지정해준다.
+ */
