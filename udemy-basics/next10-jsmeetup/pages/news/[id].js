9292b06bb077b3a885a3b5da90a54539e2abdbbc
Ina-dang <ina9377@gmail.com>
2023-07-13 19:54:27 +0900

    버튼추가 및 연습 종료

--- udemy-basics/next10-jsmeetup/pages/news/[id].js
+++ udemy-basics/next10-jsmeetup/pages/news/[id].js
@@ -1,5 +1,6 @@
 //함수형 컴포넌트에서 url 가져오기
 import { useRouter } from 'next/router';
+import { Fragment } from 'react';
 
 const DetailPage = () => {
   const router = useRouter();
@@ -7,7 +8,14 @@ const DetailPage = () => {
 
   //요청, 응답 api 연동 여기서 가능
 
-  return <h1>DetailPage {id} </h1>;
+  return (
+    <Fragment>
+      <h1>DetailPage {id} </h1>
+      <button>
+        <a href='/news'>목록</a>
+      </button>
+    </Fragment>
+  );
 };
 
 export default DetailPage;
