25be6c60ca32291b2bfd223523f5399ff8ae50d3
Ina-dang <ina9377@gmail.com>
2023-07-13 20:18:27 +0900

    메인페이지, 글작성 페이지 생성

---
+++ udemy-basics/js-meetup/pages/index.js
@@ -0,0 +1,32 @@
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: '첫번째 모임',
+    image: 'https://t1.daumcdn.net/cfile/tistory/996AD63C5A56FE5633',
+    address: '영등포구청 3번 출구',
+    description: '첫번째 모임!',
+  },
+  {
+    id: 'm2',
+    title: '두번째 모임',
+    image: 'https://thumb.mt.co.kr/06/2018/10/2018102416372894434_3.jpg',
+    address: '영등포구청 3번 출구',
+    description: '두번째 모임!',
+  },
+  {
+    id: 'm3',
+    title: '세번째 모임',
+    image:
+      'https://ichef.bbci.co.uk/news/640/cpsprodpb/1784B/production/_110113369_japandrinkinggetty.jpg',
+    address: '영등포구청 3번 출구',
+    description: '세번째 모임!',
+  },
+];
+
+const HomePage = () => {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+};
+
+export default HomePage;
