7f4c8eaa3f468d4e9056ce3b6cf26c2c32a83c8b
Ina-dang <ina9377@gmail.com>
2023-07-13 19:52:57 +0900

    Link연결

--- udemy-basics/next10-jsmeetup/pages/news/index.js
+++ udemy-basics/next10-jsmeetup/pages/news/index.js
@@ -1,5 +1,17 @@
+import { Fragment } from 'react';
+import Link from 'next/link';
+
 const NewsPage = () => {
-  return <h1>NewsPage</h1>;
+  return (
+    <Fragment>
+      <h1>NewsPage</h1>;
+      <ul>
+        <li>
+          <Link href='/news/1'>타입스크립트공부</Link>
+        </li>
+      </ul>
+    </Fragment>
+  );
 };
 
 export default NewsPage;
