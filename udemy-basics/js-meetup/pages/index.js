2d28edd0fa9643d0052b3079f7494cd8b5e7c4d7
Ina-dang <ina9377@gmail.com>
2023-07-13 21:28:49 +0900

    SSR 작성

--- udemy-basics/js-meetup/pages/index.js
+++ udemy-basics/js-meetup/pages/index.js
@@ -29,16 +29,29 @@ const HomePage = (props) => {
   return <MeetupList meetups={props.meetups} />;
 };
 
-//SSG-사전랜더링
-export async function getStaticProps() {
-  //클라이언트사이드에 들어가지 않음. 빌드 프로세스중에 실행되기때문
+//SSR 서버요청
+export async function getServerSideProps(context) {
+  const req = context.req;
+  const res = context.res;
+
   //fetch data from an API ...
   return {
     props: {
       meetups: DUMMY_MEETUPS,
     },
-    revalidate: 10, //점진적 정적 생성 기능 (regenerate) 페이지에 요청이 들어오면 10초마다 서버에서 페이지 다시 생성
-  }; //꼭 객체를 반환해야함 주로 프롭스 반환
+  };
 }
+
+//SSG-사전랜더링
+// export async function getStaticProps() {
+//   //클라이언트사이드에 들어가지 않음. 빌드 프로세스중에 실행되기때문
+//   //fetch data from an API ...
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS,
+//     },
+//     revalidate: 10, //점진적 정적 생성 기능 (regenerate) 페이지에 요청이 들어오면 10초마다 서버에서 페이지 다시 생성
+//   }; //꼭 객체를 반환해야함 주로 프롭스 반환
+// }
 
 export default HomePage;
