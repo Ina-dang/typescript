e31722731c244d3bb7e4fb065ec0eb383b0711cb
Ina-dang <ina9377@gmail.com>
2023-07-16 20:25:25 +0900

    tsc컴파일과 브라우저 자바스크립트 출력방식 확인

--- ts-basic/app.ts
+++ ts-basic/app.ts
@@ -1,1 +1,11 @@
-console.log('코드는 여기에서!');
+console.log('코드는 여기에서!!!');
+function add(a: number, b: number) {
+  return a + b;
+}
+
+const number1 = 5;
+const number2 = 12.3;
+
+const result = add(number1, number2);
+
+console.log(result);
