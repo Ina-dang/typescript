054925b4a0063b1562477c94e018c4a20fce3ea1
Ina-dang <ina9377@gmail.com>
2023-07-17 20:44:13 +0900

    열거, 이넘, 애니

--- ts-basic/app.ts
+++ ts-basic/app.ts
@@ -32,5 +32,36 @@ for (const hobby of person.hobbies) {
 // console.log(person.nickname) //Property 'nickname' does not exist on type '{ name: string; age: number; }'.ts(2339)
 
 /**
- * @description 튜플 작업하기
+ * @description 튜플 작업하기 => 자바스크립트엔 없지만 길이가 고정된 배열
  */
+const tuplePerson: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
+  name: 'Inadang',
+  age: 30,
+  hobbies: favoriteActivities,
+  role: [2, 'author'],
+};
+
+// tuplePerson.role.push('admin'); //Property 'role' does not exist on type '{ name: string; age: number; hobbies: string[]; }'.ts(2339)
+tuplePerson.role[0] = 10;
+
+/**
+ * @description enum
+ */
+
+enum Role {
+  ADMIN = 5,
+  READ_ONLY = 100,
+  AUTHOR = 200,
+}
+const role = Role.ADMIN;
+console.log(role);
+
+let sign: any;
+sign = 234;
+sign = '234';
+sign = { addresss: '234' };
