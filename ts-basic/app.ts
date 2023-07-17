b093f0d6550033a204aaa7f138a91d99dd8dd115
Ina-dang <ina9377@gmail.com>
2023-07-17 20:26:38 +0900

    기본형, 객체, 배열 연습

--- ts-basic/app.ts
+++ ts-basic/app.ts
@@ -1,11 +1,36 @@
 console.log('코드는 여기에서!!!');
 function add(a: number, b: number) {
+  // console.log(typeof number1);
   return a + b;
 }
 
-const number1 = 5;
+const number1 = 5; //5.0
 const number2 = 12.3;
 
 const result = add(number1, number2);
 
-console.log(result);
+// console.log(result);
+
+let favoriteActivities: string[];
+// favoriteActivities = [1,2,3] //Type 'number' is not assignable to type 'string'.ts(2322)
+favoriteActivities = ['Game', 'Boarding'];
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+const person = {
+  name: 'Inadang',
+  age: 30,
+  hobbies: favoriteActivities,
+};
+
+for (const hobby of person.hobbies) {
+  console.log(hobby);
+  // console.log(hobby.map()); //Property 'map' does not exist on type 'string'.ts(2339)
+}
+
+// console.log(person.nickname) //Property 'nickname' does not exist on type '{ name: string; age: number; }'.ts(2339)
+
+/**
+ * @description 튜플 작업하기
+ */
