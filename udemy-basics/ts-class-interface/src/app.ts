969c49778a4e560eae6b84f070d6eb90b2c450c4
Ina-dang <ina9377@gmail.com>
2023-07-21 06:39:14 +0900

    생성자 추가 및 private 사용

--- udemy-basics/ts-class-interface/src/app.ts
+++ udemy-basics/ts-class-interface/src/app.ts
@@ -4,11 +4,43 @@
 
 class Department {
   name: string;
+  private employees: string[] = [];
+
   constructor(n: string) {
     this.name = n;
+  }
+
+  describe(this: Department) {
+    // console.log('Department: ', age); //Cannot find name 'age'.ts(2304)
+    console.log('Department: ', this.name);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
   }
 }
 
 const accounting = new Department('Accounting');
 
 console.log(accounting);
+accounting.describe();
+
+// const accountingCopy = { describe: accounting.describe };
+// accountingCopy.describe(); //undefined
+// const accountingCopy = { describe: accounting.describe() };
+// accountingCopy.describe;
+// const accountingCopy = { name: 'Laura', describe: accounting.describe };
+// accountingCopy.describe();
+
+accounting.addEmployee('Mark');
+accounting.addEmployee('Jax');
+
+// accounting.employees[2] = 'Anna'; //Property 'employees' is private and only accessible within class 'Department'.ts(2341)
+
+accounting.describe();
+accounting.printEmployeeInformation();
