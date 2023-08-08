7c458d6e442090e3e122687ecc2f530ec5d12508
INA nh.lee <ina9377@gmail.com>
2023-08-09 06:41:46 +0900

    클래스 데코레이터에서 클래스 반환하기(제네릭)

--- udemy-basics/ts-class-interface/src/app.ts
+++ udemy-basics/ts-class-interface/src/app.ts
@@ -1,5 +1,5 @@
 //데코레이터 => 실체화 되기 전 클래스가 정의만 되어있어도 실행된다. 실체화를 삭제해도 작동된다.
-//클래스, 메소드, 인자등 다양한 데코레이터 패턴으로 사용할 수 있다.
+//클래스, 템플릿, 메소드, 인자등 다양한 데코레이터 패턴으로 사용할 수 있다.
 
 //Logger함수를 퍼스트 클래스 데코레이터로 만들기
 // function Logger(constructor: Function) {
@@ -10,26 +10,33 @@
 // 데코레이터 팩토리 작업
 function Logger(logString: string) {
   return function (constructor: Function) {
-    console.log('LOGGIN-FACTORY');
+    console.log('[LOGGIN-FACTORY]');
     console.log(logString);
     console.log('constructor:: ', constructor);
   };
 }
 
 function WithTemplate(template: string, hookId: string) {
-  return function (constructor: any) {
-    console.log('TEMPLATE-FACTORY');
-    //타입스크립트 시그널(_)추가
-    const hookEl = document.getElementById(hookId);
-    const p = new constructor();
-    if (hookEl) {
-      hookEl.innerHTML = template;
-      hookEl.querySelector('h1')!.textContent = p.name;
-    }
+  console.log('[TEMPLATE-FACTORY]');
+  return function <T extends { new (...args: any[]): { name: string } }>(
+    originalConstructor: T
+  ) {
+    //데코레이터 반환값 추가 하여 새 컨스트럭터나 새 클래스, 새 함수 반환가능
+    return class extends originalConstructor {
+      constructor(..._: any[]) {
+        super();
+        console.log('Rendering template');
+        const hookEl = document.getElementById(hookId);
+        if (hookEl) {
+          hookEl.innerHTML = template;
+          hookEl.querySelector('h1')!.textContent = this.name;
+        }
+      }
+    };
   };
 }
 
-@Logger('LOGGING-PERSON') //식별자 어노테이션
+// @Logger('LOGGING-PERSON') //식별자 어노테이션
 @WithTemplate('<h1>My Person Object</h1>', 'app')
 class Person {
   name = 'Max';
