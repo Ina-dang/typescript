e94f941bb7c30b2225a4dee4fe8603ba3f914745
INA nh.lee <ina9377@gmail.com>
2023-08-14 06:52:24 +0900

    폼제출 콘솔확인, autobind 데코레이터 추가

--- udemy-basics/ts-drag-n-drop/src/app.ts
+++ udemy-basics/ts-drag-n-drop/src/app.ts
@@ -1,7 +1,24 @@
+//autobind decorator
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
+
+//ProjectInput Class
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
     this.templateElement = document.getElementById(
@@ -14,7 +31,31 @@ class ProjectInput {
       true
     );
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = 'user-input';
+
+    this.titleInputElement = this.element.querySelector(
+      '#title'
+    ) as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      '#description'
+    ) as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector(
+      '#people'
+    ) as HTMLInputElement;
+    this.configure();
     this.render();
+  }
+
+  @autobind
+  private submintHandler(event: Event) {
+    // 폼제출 및 유효성검사
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure() {
+    // this.element.addEventListener('submit', this.submintHandler.bind(this)); // autobind 데코레이터 없을 때 사용
+    this.element.addEventListener('submit', this.submintHandler);
   }
 
   private render() {
