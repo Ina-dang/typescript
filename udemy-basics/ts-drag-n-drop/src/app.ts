aa9032d1e66d547315ea795483a30104595fa185
INA nh.lee <ina9377@gmail.com>
2023-08-14 06:36:09 +0900

    랜더링 하도록 DOM 잡은 후 css 수정

---
+++ udemy-basics/ts-drag-n-drop/src/app.ts
@@ -0,0 +1,26 @@
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+
+  constructor() {
+    this.templateElement = document.getElementById(
+      'project-input'
+    ) as HTMLTemplateElement;
+    this.hostElement = document.getElementById('app') as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.render();
+  }
+
+  private render() {
+    //HTML삽입
+    this.hostElement.insertAdjacentElement('afterbegin', this.element);
+  }
+}
+
+const projectInput = new ProjectInput();
