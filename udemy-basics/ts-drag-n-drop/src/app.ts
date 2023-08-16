1c408e4486846b8dfef8f3b4116b89ce72bb8d1c
INA nh.lee <ina9377@gmail.com>
2023-08-16 16:14:36 +0900

    status에 따라 필터링

--- udemy-basics/ts-drag-n-drop/src/app.ts
+++ udemy-basics/ts-drag-n-drop/src/app.ts
@@ -9,7 +9,7 @@ class Project {
     public title: string,
     public description: string,
     public people: number,
-    public state: ProjectStatus
+    public status: ProjectStatus
   ) {}
 }
 
@@ -41,7 +41,6 @@ class ProjectState {
       numOfPeople,
       ProjectStatus.Active
     );
-    this.projects = [];
     this.projects.push(newProject);
     for (const listenerFn of this.listeners) {
       listenerFn(this.projects.slice());
@@ -132,7 +131,13 @@ class ProjectList {
     this.element.id = `${this.type}-projects`;
 
     projectState.addListener((projects: Project[]) => {
-      this.assignedProjects = projects;
+      const relevantProjects = projects.filter((prj) => {
+        if (this.type === 'active') {
+          return prj.status === ProjectStatus.Active;
+        }
+        return prj.status === ProjectStatus.Finished;
+      });
+      this.assignedProjects = relevantProjects;
       this.renderProjects();
     });
 
@@ -143,6 +148,9 @@ class ProjectList {
     const listEl = document.getElementById(
       `${this.type}-projects-list`
     )! as HTMLUListElement;
+
+    listEl.innerHTML = '';
+
     for (const projectItem of this.assignedProjects) {
       const listItem = document.createElement('li');
       listItem.textContent = projectItem.title;
