d9df2a92e3fab93aa6e004ccc51c418af85d8003
Ina <ina9377@gmail.com>
2024-02-26 22:49:55 +0900

    타입 수정

--- pokemon-rtk/src/types/Poke.ts
+++ pokemon-rtk/src/types/Poke.ts
@@ -1,22 +1,25 @@
-export interface Poke {
+export type Poke = {
   id: number;
   name: string;
   species: {
     name: string;
+    // species와 관련된 다른 속성들을 여기에 추가할 수 있습니다.
   };
   sprites: {
     front_shiny: string;
+    // 다른 스프라이트 이미지와 관련된 속성들을 여기에 추가할 수 있습니다.
   };
-}
+  results: PokemonListResponse;
+};
 
-export interface PokemonListResponse {
+type PokemonListResponse = {
   count: number;
   next: string | null;
   previous: string | null;
   results: PokemonListItem[];
-}
+};
 
-export interface PokemonListItem {
+type PokemonListItem = {
   name: string;
   url: string;
-}
+};
